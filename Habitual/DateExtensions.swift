4b9bf7e43468d2d7aea23023d1cd3626bb8c6bd8
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-03-01 11:44:37 -0800

    Page 5

---
+++ Habitual/DateExtensions.swift
@@ -0,0 +1,25 @@
+//
+//  DateExtensions.swift
+//  Habitual
+//
+//  Created by Angelina Olmedo on 3/1/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    var stringValue: String {
+        return DateFormatter.localizedString(from: self, dateStyle: .medium, timeStyle: .none)
+    }
+
+    var isToday: Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInToday(self)
+    }
+
+    var isYesterday: Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInYesterday(self)
+    }
+}
--- Habitual/Views/HabitsTableViewController.swift
+++ Habitual/Views/HabitsTableViewController.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
+//struct Habit { // won't recognize in the other file for some reason :/
+//    var title: String
+//}
+
 class HabitsTableViewController: UITableViewController {
     
-    var names: [String] = ["Alan", "Braus", "Adriana", "Mitchell", "Dani", "Jess", "Dan", "Meredith", "Dan", "Milad"]
+    var habits: [Habit] = [
+        Habit(title: "Go to bed before 10pm", image: Habit.Images.book),
+        Habit(title: "Drink 8 Glasses of Water", image: Habit.Images.book),
+        Habit(title: "Commit Today", image: Habit.Images.book),
+        Habit(title: "Stand up every Hour", image: Habit.Images.book)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,21 +29,31 @@ class HabitsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+
+      return habits.count
+
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell: UITableViewCell
-        if let dequeueCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
-            cell = dequeueCell
+
+      var cell: UITableViewCell
+
+      if let dequeueCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+
+        cell = dequeueCell
+
         } else {
-            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        }
 
-        let name = names[indexPath.row]
-        cell.textLabel?.text = name
+        cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
 
-        return cell
+      }
+
+      let habit = habits[indexPath.row]
+
+      cell.textLabel?.text = habit.title
+
+      return cell
+
     }
 }
 
@@ -47,8 +66,8 @@ extension HabitsTableViewController {
     }
 
     @objc func pressAddHabit(_ sender: UIBarButtonItem) {
-        names.insert("Hello, World!", at: 0)
-        let topIndexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [topIndexPath], with: .automatic)
+//        habits.insert("Hello, World!", at: 0)
+//        let topIndexPath = IndexPath(row: 0, section: 0)
+//        tableView.insertRows(at: [topIndexPath], with: .automatic)
     }
 }
