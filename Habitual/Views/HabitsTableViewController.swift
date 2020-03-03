6c31d1954f7f05d0cbfc97049436fc12cadc41f2
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-03-02 19:19:24 -0800

    Finished!

--- Habitual/Views/HabitsTableViewController.swift
+++ Habitual/Views/HabitsTableViewController.swift
@@ -54,6 +54,27 @@ class HabitsTableViewController: UITableViewController {
       return cell
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+          case .delete:
+            let habitToDelete = persistence.habits[indexPath.row]
+            let habitIndexToDelete = indexPath.row
+
+            let deleteAlert = UIAlertController(habitTitle: habitToDelete.title) {
+              self.persistence.delete(habitIndexToDelete)
+              tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+
+            self.present(deleteAlert, animated: true)
+          default:
+            break
+          }
+      }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+      persistence.swapHabits(habitIndex: sourceIndexPath.row, destinationIndex: destinationIndexPath.row)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
@@ -68,6 +89,7 @@ extension HabitsTableViewController {
         title = "Habitual"
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(pressAddHabit(_:)))
         navigationItem.rightBarButtonItem = addButton
+        navigationItem.leftBarButtonItem = self.editButtonItem
     }
 
     @objc func pressAddHabit(_ sender: UIBarButtonItem) {
@@ -75,5 +97,19 @@ extension HabitsTableViewController {
       let navigationController = UINavigationController(rootViewController: addHabitVC)
       navigationController.modalPresentationStyle = .fullScreen
       present(navigationController, animated: true, completion: nil)
+    }
+}
+
+extension UIAlertController {
+    convenience init(habitTitle: String, comfirmHandler: @escaping () -> Void) {
+        self.init(title: "Delete Habit", message: "Are you sure you want to delete \(habitTitle)?", preferredStyle: .actionSheet)
+
+        let confirmAction = UIAlertAction(title: "Confirm", style: .destructive) { _ in
+            comfirmHandler()
+        }
+        self.addAction(confirmAction)
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        self.addAction(cancelAction)
     }
 }
